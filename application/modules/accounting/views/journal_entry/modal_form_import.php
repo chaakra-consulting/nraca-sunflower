a941fb53a94728b66ec22912aa6ae3e68c739430
ikiraihan <ikiraihann@gmail.com>
2025-06-12 09:24:12 +0700

    add file template excel

--- application/modules/accounting/views/journal_entry/modal_form_import.php
+++ application/modules/accounting/views/journal_entry/modal_form_import.php
@@ -11,7 +11,7 @@
             <div class="form-group">
                 <label for="file" class="col-md-3">Upload Excel</label>
                 <div class="col-md-12">
-                <?php
+                    <?php
                     echo form_input(array(
                         "id" => "file",
                         "name" => "file",
@@ -19,8 +19,19 @@
                         "type" => "file",
                         "accept" => ".xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                     ));
-                ?>
+                    ?>
                 </div>
+            </div>
+            <div class="col-md-12 mb10">
+                    <p class="text-danger">
+                        <strong>Catatan:</strong><br>
+                        1. Nama sheet di file Excel <strong>wajib</strong> bernama <code>Data Penjualan</code><br>
+                        2. Urutan kolom pada file Excel <strong>harus sesuai dengan format template</strong><br>
+                        Silakan unduh contoh template di sini:
+                        <a href="<?php echo get_uri("files/template_excel/Contoh_Import.xlsx"); ?>" target="_blank">
+                            Download Template
+                        </a>
+                    </p>
             </div>
         </div>
     </div>
